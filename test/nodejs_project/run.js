c545774c3a920bfadda6209b5669f83d16c21787
daipeng <1227151030@qq.com>
2021-03-05 16:49:30 +0800

    init

---
+++ test/nodejs_project/run.js
@@ -0,0 +1,5 @@
+var IconfontWebpackPlugin = require('../../src/index');
+
+var options = require('../iconfont-options');
+
+new IconfontWebpackPlugin(options).build()
---
+++ test/web_project/src/main.js
@@ -0,0 +1,18 @@
+require('./fonts/font.css');
+import fontList from './fonts/fonts.js';
+
+
+window.onload = function () {
+    var html = fontList.map(function (font) {
+        return '<section>' +
+            '<header contenteditable>my-icon-' + font.name + '</header>' +
+            '<div>' +
+            '    <i class="my-icon-' + font.name + '"></i>' +
+            '</div>' +
+            '</section>'
+    }).join('')
+
+    document.querySelector('.flex-tb').innerHTML = html
+}
+
+
